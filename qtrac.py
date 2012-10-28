d214f80f3858c3db25b197f1495c7d580a60ccc9
Yasar Senturk <yasar@senturk.name.tr>
2012-10-28 06:25:03 -0400

    Showing MainWindow in foreground on Mac OS X

--- qtrac.py
+++ qtrac.py
@@ -8,15 +8,17 @@ from PyQt4 import QtGui
 
 from controller.MainWindow import MainWindow
 
-isMACOSX = hasattr(QtGui, "qt_mac_set_native_menubar")
+isMACOSX = False
+if sys.platform == 'darwin':
+	isMACOSX = True 
 
 qtrac = QtGui.QApplication(sys.argv)
 main_window = QtGui.QMainWindow()
 my_main_window = MainWindow()
 my_main_window.show()
 
+''' Show MainWindow in foreground on Mac OS X  '''
+if isMACOSX:
+	main_window.raise_()
 
 sys.exit(qtrac.exec_())
-
-'''
-'''
